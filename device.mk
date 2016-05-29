ed2413fd424921f60bcf799501c90bb748c30baf
lawn <bloom.all.over.12@gmail.com>
2016-05-30 04:06:14 +0900

    Support felica

--- device.mk
+++ device.mk
@@ -125,7 +125,9 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.carrier.rc \
+    init.felica.sh
 
 # Thermal
 PRODUCT_COPY_FILES += \
