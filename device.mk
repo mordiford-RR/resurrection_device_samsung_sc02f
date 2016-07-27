55b5419a28dccfb832329237f42d372a2f8af1e5
lawn <bloom.all.over.12@gmail.com>
2016-07-28 05:42:34 +0900

    RR: sc02f: Enable Hardware key pref Your original build script need this flag option
    export TARGET_CUSTOM_ROM=RR

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ $(call inherit-product-if-exists, vendor/samsung/sc02f/sc02f-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+ifeq ($(TARGET_CUSTOM_ROM),RR)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay_rr
+endif
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
